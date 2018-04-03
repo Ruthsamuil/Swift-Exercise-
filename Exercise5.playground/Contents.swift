dcc4ba57d70dc8e8bcf00c6875a29d1b0b9b807f
Ruthsamuil <37755706+Ruthsamuil@users.noreply.github.com>
2018-04-03 13:53:11 +0200

    Add files via upload

---
+++ Exercise5.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var Value = [1,12,5,10]
+
+var result = Value.sorted (by: { $0 < $1 })
+
+print("result = \(result)")
+
+
+
+
+
+
+
+
+		
